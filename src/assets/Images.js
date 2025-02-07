53a2b6afe48ba7969bfb54140d1d52164216ebc9
muthukumarniven <rrmuthukumar007@gmail.com>
2025-02-07 12:24:35 +0530

    add image

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -3,7 +3,9 @@ import avatar from './images/avatar-img.jpg'
 import edit from './images/edit.svg'
 
 
+import avatarTwo from './images/avatar-2.png'
 
+import avatarThree from './images/avatar-3.jpg'
 
 
 
@@ -15,6 +17,8 @@ const Images = {
     speak: require('./images/speak.svg').default,
     edit: require('./images/edit.svg').default, 
     avatar,
+    avatarTwo,
+    avatarThree
     
 
     
