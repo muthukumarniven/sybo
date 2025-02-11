e5e0d7ea6ba6e7b90838121a456cd26db87c2e65
muthukumarniven <rrmuthukumar007@gmail.com>
2025-02-11 17:02:23 +0530

    add gif styles

--- src/component/Gif.js
+++ src/component/Gif.js
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from 'react';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const Gif = () => {
-    return (
-        <>
-
-            <DotLottieReact
-                src="https://lottie.host/643726a5-e034-4148-a525-077418c5f81b/V9w5X8H79K.lottie"
-                loop
-                autoplay
-                color='#76BEE9'
-                style={{width:"217px", height:"117px"}}
-            />
-
-        </>
-    )
+  return (
+    <>
+      <DotLottieReact
+        src="https://lottie.host/643726a5-e034-4148-a525-077418c5f81b/V9w5X8H79K.lottie"
+        loop
+        autoplay
+        color='#76BEE9'
+        style={{ maxWidth: "217px", maxHeight: "117px" }}  // Optional: Add your custom size
+      />
+    </>
+  );
 }
 
-export default Gif
+export default Gif;
