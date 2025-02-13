eb6c12941a05e3ae4e13a7655582707125530dac
muthukumarniven <rrmuthukumar007@gmail.com>
2025-02-13 11:50:09 +0530

    add content

--- src/screens/QuestionAvatarVideo.js
+++ src/screens/QuestionAvatarVideo.js
@@ -4,8 +4,8 @@ import myVideo from "../assets/videos/founders-video.mp4"; // Import the video
 const QuestionAvatarVideo = () => {
     return (
         // <div className='question-one-avatar'>
-        <div style={{height:"100vh"}} className='avatarr-v'>
-            <video style={{height:"100%"}} width="100%" height="auto" controls>
+        <div style={{ height: "100vh" }} className='avatarr-v'>
+            <video style={{ height: "100%" }} width="100%" height="auto" controls>
                 <source src={myVideo} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
--- src/screens/QuestionOne.js
+++ src/screens/QuestionOne.js
@@ -18,8 +18,8 @@ const QuestionOne = () => {
 
 
     return (
-        <div className='question-one-avatar'>
-            <div className='bottom-navbar-header'>
+        <div className='chat-container'>
+            <div className='message'>
                 <div className='ai-chat-container d-flex flex-column gap-3'>
                     <div className='d-flex align-items-end justify-content-start gap-2'>
                         <div className='ai-chat-item'>
@@ -35,53 +35,22 @@ const QuestionOne = () => {
                             <p className='time-text-content mb-0 text-end mt-1'>9.23 PM</p>
                         </div>
                     </div>
-
-                    <div className='d-flex align-items-end justify-content-start gap-2'>
-                        <div className='ai-chat-item'>
-                            <p className='mb-0'>What specific challenge are you facing
-                                right now?</p>
-                            <p className='time-text-content mb-0 text-end mt-1'>9.23 PM</p>
-
-                        </div>
-                    </div>
-              
-                    <div className='d-flex align-items-end justify-content-end gap-2'>
-                        <Button backgroundColor='#D9D9D9' buttonWidth='23px' buttonHeight='23px' imgWidth='8px' imgHeight='8px' imageSrc={Images.edit} />
-                        <div className='user-chat-item'>
-                            <p className='mb-0'>Relationship</p>
-                            <p className='time-text-content mb-0 text-end mt-1'>9.23 PM</p>
-                        </div>
-                    </div>
-                    <div className='d-flex align-items-end justify-content-start gap-2'>
-                        <div className='ai-chat-item'>
-                            <p className='mb-0'>What specific challenge are you facing
-                                right now?</p>
-                            <p className='time-text-content mb-0 text-end mt-1'>9.23 PM</p>
-
-                        </div>
-                    </div>
-                    <div className='d-flex align-items-end justify-content-end gap-2'>
-                        <Button backgroundColor='#D9D9D9' buttonWidth='23px' buttonHeight='23px' imgWidth='8px' imgHeight='8px' imageSrc={Images.edit} />
-                        <div className='user-chat-item'>
-                            <p className='mb-0'>Relationship</p>
-                            <p className='time-text-content mb-0 text-end mt-1'>9.23 PM</p>
-                        </div>
-                    </div>
-
-
-
                 </div>
-                <div className='listening-content d-flex align-items-center justify-content-center gap-3'>
+            </div>
+            <div className='listening-content d-flex align-items-center justify-content-center gap-3'>
                     <p className='mb-0'>listening</p>
                     <img className='gif-content' src={Images.listeningGif} alt="Sybo GIF" />
 
                 </div>
-                <div className='d-flex align-items-center justify-content-center gap-3 bottom-navbar-conatiner'>
-                    <Input imageSrc={Images.newImage} marginTop='0px' placeholder='Write a message...' onChange={handleFullNameChange} />
-                    <Button onClick={handleClick} imageSrc={Images.speak} />
-                </div>
+            <div className='d-flex align-items-center justify-content-center gap-3 bottom-navbar-conatiner'>
+                <Input imageSrc={Images.newImage} marginTop='0px' placeholder='Write a message...' onChange={handleFullNameChange} />
+                <Button onClick={handleClick} imageSrc={Images.speak} />
             </div>
         </div>
+
+
+
+
     )
 }
 
